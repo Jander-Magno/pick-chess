f3fee947499a8ed3380d542a592ca6be9e28d07d
MagnoYo <juniormagnoreis@outlook.com>
2025-04-12 12:01:11 +0100

    Class Created.

--- pick-chess/Program.cs
+++ pick-chess/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace pick_chess
+﻿using pick_chess.Board;
+
+namespace pick_chess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
+
+            Console.ReadLine();
         }
     }
 }
