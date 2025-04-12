a033d6308611eb75be2301352196dedfadc19144
MagnoYo <juniormagnoreis@outlook.com>
2025-04-12 11:53:00 +0100

    Adicionar arquivos de projeto.

---
+++ pick-chess/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pick_chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
